4955f20fe7154f56fc58dba1ec8fbab9ae1e123b
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 18:23:37 +0300

    Add files via upload

---
+++ matlab/initializationBoTW.m
@@ -0,0 +1,36 @@
+%
+
+% Copyright 2020, Konstantinos A. Tsintotas
+% ktsintot@pme.duth.gr
+%
+% This file is part of openBoTW framework for visual loop closure detection
+%
+% openBoTW framework is free software: you can redistribute 
+% it and/or modify it under the terms of the MIT License as 
+% published by the corresponding authors.
+%  
+% openBoTW pipeline is distributed in the hope that it will be 
+% useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% MIT License for more details. <https://opensource.org/licenses/MIT>
+
+function BoTW = initializationBoTW(visualData, params)
+        
+        % the visual dictionary
+        BoTW.bagOfTrackedWords = single(zeros(100000, params.incomingVisualData.descriptorDimension));
+        % tracked words' indexing
+        BoTW.twIndex = uint16(zeros(100000, 3));
+        % tracked words' location indexing
+        BoTW.twLocationIndex = false(100000, size(visualData.inputImage, 2));
+        % the deteled words
+        BoTW.deleted = uint16(0);
+        % lamda is counting the number of tracked words belonging to the traversed location
+        BoTW.lamda = zeros(1, size(visualData.inputImage, 2), 'uint16');
+        % maximum active point variable for the query process
+        BoTW.maximumActivePoint = zeros(1, size(visualData.inputImage, 2), 'uint16');
+        % query points
+        BoTW.queryPoints = cell(1, visualData.imagesLoaded);
+        % query descriptors
+        BoTW.queryDescriptors = cell(1, visualData.imagesLoaded);
+        
+end
