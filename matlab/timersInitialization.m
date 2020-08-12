4de2ca87f0796153b23481c0d3a7808c6442f9d5
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 18:10:44 +0300

    Update timersInitialization.m

--- matlab/timersInitialization.m
+++ matlab/timersInitialization.m
@@ -1,16 +1,33 @@
+% 
+
+% Copyright 2019, Konstantinos Tsintotas
+% ktsintot@pme.duth.gr
+%
+% This file is part of Bag-of-Tracked-Words framework for loop closure
+% detection
+%
+% Bag-of-Tracked-Words framework is free software: you can redistribute 
+% it and/or modify it under the terms of the MIT License as 
+% published by the corresponding authors.
+%  
+% Bag-of-Tracked-Words pipeline is distributed in the hope that it will be 
+% useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% MIT License for more details. <https://opensource.org/licenses/MIT>
+
 function timer = timersInitialization(visualData, timer)
 
-    %  memory allocation for timer for feature tracking
+    %  memory allocation for feature tracking timer
     timer.trackingPoints = zeros(visualData.imagesLoaded, 1,'single');
-    % memory allocation for timer for guided feature selection
+    % memory allocation for guided feature selection timer
     timer.guidedFeatureSelection = zeros(visualData.imagesLoaded, 1,'single');
-    % memory allocation for timer for brute force database search
+    % memory allocation for brute force database search timer
     timer.databaseSearch = zeros(visualData.imagesLoaded, 1,'single');
-    % memory allocation for timer for votes distribution
+    % memory allocation for votes distribution timer
     timer.votesDistribution = zeros(visualData.imagesLoaded, 1,'single');
-    % memory allocation for timer for binomial scoring
+    % memory allocation for binomial scoring timer
     timer.binomialScoring = zeros(visualData.imagesLoaded, 1,'single');
-    % memory allocation for timer for geometrical verification 
+    % memory allocation for geometrical verification timer
     timer.geometricalVerification = zeros(visualData.imagesLoaded, 1,'single');    
     
 end
