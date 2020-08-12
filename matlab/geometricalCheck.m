bf722e0ff76df86849018e28dee9f432abe0e880
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 18:35:33 +0300

    Update geometricalCheck.m

--- matlab/geometricalCheck.m
+++ matlab/geometricalCheck.m
@@ -3,13 +3,14 @@
 % Copyright 2019, Konstantinos Tsintotas
 % ktsintot@pme.duth.gr
 %
-% This file is part of iBoTW framework for visual loop closure detection
+% This file is part of Bag-of-Tracked-Words framework for loop closure
+% detection
 %
-% iBoTW framework is free software: you can redistribute 
+% Bag-of-Tracked-Words framework is free software: you can redistribute 
 % it and/or modify it under the terms of the MIT License as 
 % published by the corresponding authors.
 %  
-% iBoTW pipeline is distributed in the hope that it will be 
+% Bag-of-Tracked-Words pipeline is distributed in the hope that it will be 
 % useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
