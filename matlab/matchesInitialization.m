5cce555073c6d9fa8328e2a6e78316142d57f315
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 18:46:12 +0300

    Update matchesInitialization.m

--- matlab/matchesInitialization.m
+++ matlab/matchesInitialization.m
@@ -1,15 +1,16 @@
 % 
 
-% Copyright 2019, Konstantinos A. Tsintotas
+% Copyright 2019, Konstantinos Tsintotas
 % ktsintot@pme.duth.gr
 %
-% This file is part of HMM-BoTW framework for visual loop closure detection
+% This file is part of Bag-of-Tracked-Words framework for loop closure
+% detection
 %
-% HMM-BoTW framework is free software: you can redistribute 
+% Bag-of-Tracked-Words framework is free software: you can redistribute 
 % it and/or modify it under the terms of the MIT License as 
 % published by the corresponding authors.
 %  
-% HMM-BoTW pipeline is distributed in the hope that it will be 
+% Bag-of-Tracked-Words pipeline is distributed in the hope that it will be 
 % useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
@@ -26,7 +27,5 @@ function matches = matchesInitialization(visualData, params)
         matches.matches = zeros(visualData.imagesLoaded, 1, 'single');
         % inliers from geometrical check of the selected image
         matches.inliers = zeros(visualData.imagesLoaded, 1, 'uint16');
-        % hold the nearest neighbor of each query descriptor to the vocabulary
-        matches.knnIDx = zeros(params.buildingDatabase.numPointsToTrack, visualData.imagesLoaded, 'uint16');
         
 end
