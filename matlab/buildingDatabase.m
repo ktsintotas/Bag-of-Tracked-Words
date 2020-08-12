dfc6610c814c79893d653cdf519dc64fd633aaaa
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 18:23:01 +0300

    Update buildingDatabase.m

--- matlab/buildingDatabase.m
+++ matlab/buildingDatabase.m
@@ -15,124 +15,122 @@
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
 
-function BoTW = buildingDatabase(visualData, params)
+function [BoTW, timer] = buildingDatabase(visualData, params, timer)
 
     % shall we load the visual information and its' extracted variables?
     if params.buildingDatabase.load == true && exist('results/buildingDatabase.mat', 'file')    
         load('results/buildingDatabase.mat'); 
     
-    else       
+    else
+        
         % tracked words' counter
         twCounter = single(0);
-        
-        % visual dictionary
-        BoTW.bagOfTrackedWords = single([]);
-        % tracked words' indexing
-        BoTW.twIndex = int16([]);
-        % lamda is counting the number of tracked words belonging to the traversed location
-        BoTW.lamda = zeros(1, size(visualData.inputImage, 2), 'int16');
-        % maximum active point variable for the query process
-        BoTW.maximumActivePoint = zeros(1, size(visualData.inputImage, 2), 'int16');
-        % query points
-        BoTW.queryPoints = cell(1, visualData.imagesLoaded);
-        % query descriptors
-        BoTW.queryDescriptors = cell(1, visualData.imagesLoaded);
-        
+        % giving an arbitrary amount for the Bag of Tracked Words database
+        BoTW = initializationBoTW(visualData, params);
         % initialization of points' tracking validity based on our conditions
-        trackObservation = false(params.numPointsToTrack, 1);
+        trackObservation = false(params.buildingDatabase.numPointsToTrack, 1);
         % initialization of points' representation along consecutive features
-        pointRepeatability = ones(params.numPointsToTrack, 1, 'int16');    
+        pointRepeatability = ones(params.buildingDatabase.numPointsToTrack, 1, 'uint16');
 
-        for It = int16(1 : visualData.imagesLoaded)
-
+        for It = uint16(1 : visualData.imagesLoaded)
+            
+            disp(It)
+            
             % initialization of points and descriptors   
             if It == 1                               
-                
-                if size(visualData.pointsSURF{It}, 1) > params.numPointsToTrack
+
+                if size(visualData.points{It}, 1) > params.buildingDatabase.numPointsToTrack
                     % initial query points and initial points for the tracker
-                    BoTW.queryPoints{It}= visualData.pointsSURF{It}.Location(1 : params.numPointsToTrack, :);
+                    BoTW.queryPoints{It}= visualData.points{It}.Location(1 : params.buildingDatabase.numPointsToTrack, :);
                     % initial points, each one into a cell bag
-                    trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, params.numPointsToTrack));
+                    trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, params.buildingDatabase.numPointsToTrack));
                     % initial query descriptors
-                    BoTW.queryDescriptors{It} = visualData.featuresSURF{It}(1 : params.numPointsToTrack, :);
+                    BoTW.queryDescriptors{It} = visualData.features{It}(1 : params.buildingDatabase.numPointsToTrack, :);
                     % initial descriptors, each one into a cell bag
-                    trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, params.numPointsToTrack));
+                    trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, params.buildingDatabase.numPointsToTrack));
                 else 
-                    BoTW.queryPoints{It}= visualData.pointsSURF{It}.Location(1 : size(visualData.pointsSURF{It}, 1), :);
-                    trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, size(visualData.pointsSURF{It}, 1)));                   
-                    BoTW.queryDescriptors{It} = visualData.featuresSURF{It}(1 : size(visualData.featuresSURF{It}, 1), :);
-                    trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, size(visualData.featuresSURF{It}, 1)));
+                    % initial query points and initial points for the tracker
+                    BoTW.queryPoints{It}= visualData.points{It}.Location(1 : size(visualData.points{It}, 1), :);
+                    % initial points, each one into a cell bag
+                    trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, size(visualData.points{It}, 1)));      
+                     % initial query descriptors
+                    BoTW.queryDescriptors{It} = visualData.features{It}(1 : size(visualData.features{It}, 1), :);
+                    % initial descriptors, each one into a cell bag
+                    trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, size(visualData.features{It}, 1)));
                 end    
 
                 % point tracker object generation that tracks a set of points
                 pointTracker = vision.PointTracker('NumPyramidLevels', 3, 'MaxBidirectionalError', 3); 
                 initialize(pointTracker, BoTW.queryPoints{It}, visualData.inputImage{It});
-                
+
             end
-              
+
             if It > 1
-                % I(t-1)
-                previousIt = int16(It-1);
-                
-                if ~isempty(BoTW.queryPoints{previousIt}) && size(visualData.pointsSURF{It}, 1) > params.inliersTheshold
+                previousIt = uint16(It-1);                
+                if ~isempty(BoTW.queryPoints{previousIt}) && size(visualData.points{It}, 1) > params.queryingDatabase.inliersTheshold
             
                     if It > 2                
                         % objects lock when you call them and the release function unlocks them
-                        release(pointTracker); 
+                        release(pointTracker);
                         % initialize again the tracker with the new and more accurate points            
-                        initialize(pointTracker, BoTW.queryPoints{previousIt}, visualData.inputImage{previousIt}); 
+                        initialize(pointTracker, BoTW.queryPoints{previousIt}, visualData.inputImage{previousIt});
                     end
-
-                    % A.1 POINT TRACKING
+                    
+                    % start the timer for the Kanade-Lucas-Tomase tracker
+                    tic 
                     % tracked points in the incoming frame
-                    [trackedPoints, trackedPointsValidity] = pointTracker(visualData.inputImage{It}); 
+                    [trackedPoints, trackedPointsValidity] = pointTracker(visualData.inputImage{It});
+                    % stop the timer for the Kanade-Lucas-Tomase tracker
+                    timer.trackingPoints(It, 1) = toc;
 
-                    % A.2 GUIDED GEATURE DETECTION             
-                    [BoTW.queryPoints{It}, BoTW.queryDescriptors{It}, pointRepeatability, trackObservation, pointsToSearch, descriptorsToSearch, trackedPointsBag, trackedDescriptorsBag] = ... 
-                        guidedFeatureDetection(params, visualData, previousIt, It, BoTW.queryPoints{previousIt}, BoTW.queryDescriptors{previousIt}, trackedPoints, trackedPointsValidity, pointRepeatability, trackedPointsBag, trackedDescriptorsBag, trackObservation);
-
-                    % A.3 BAG OF TRACKED WORDS
-                    newPointCounter = int16(0);
+                    % GUIDED FEATURE SELECTION
+                    [BoTW.queryPoints{It}, BoTW.queryDescriptors{It}, pointRepeatability, trackObservation, pointsToSearch, descriptorsToSearch, trackedPointsBag, trackedDescriptorsBag, timer] = ... 
+                        guidedFeatureSelection(params, visualData, previousIt, It, BoTW.queryPoints{previousIt}, BoTW.queryDescriptors{previousIt}, trackedPoints, trackedPointsValidity, pointRepeatability, trackedPointsBag, trackedDescriptorsBag, trackObservation, timer);
+                    
+                    % TRACKED WORD GENERATION
+                    newPointCounter = uint16(0);
                     deletion = false;
                     pointsToDelete = false(length(trackObservation), 1);                
-                    for tw = int16(1 : params.numPointsToTrack)
-                        
-                        % When the tracking of a certain point is discontinued, its total length measured in consecutive frames, determines whether a new word should be created
-                        if trackObservation(tw) == false && pointRepeatability(tw) > params.trackLength 
+                                         
+                    for td = uint16(1 : params.buildingDatabase.numPointsToTrack)
+   
+                        % when the tracking of a certain point is discontinued, its total length measured in consecutive frames, determines whether a new word should be created
+                        if trackObservation(td) == false && pointRepeatability(td) > params.buildingDatabase.trackLength 
                             twCounter =  twCounter + 1;
                             % Bag of Tracked Words generation from the average of the tracked descriptors the representative TW is computed
-                            BoTW.bagOfTrackedWords(twCounter, :) = mean(trackedDescriptorsBag{tw}, 1 );
+                            BoTW.bagOfTrackedWords(twCounter, :) = mean(trackedDescriptorsBag{td}, 1 );  
                             % first image where the point is observed //BoTW.twIndex(twCounter, 1) = It - pointRepeatability(tw);
                             % last image where the feature is observed // BoTW.twIndex(twCounter, 2) = previous;
                             % tracked word's repeatability // BoTW.twIndex(twCounter, 3)
-                            BoTW.twIndex(twCounter, :) = [(It - pointRepeatability(tw)), previousIt, pointRepeatability(tw)];
+                            BoTW.twIndex(twCounter, :) = [(It - pointRepeatability(td)), previousIt, pointRepeatability(td)];
+                            % track word to location binary indexing
+                            BoTW.twLocationIndex(twCounter, It - pointRepeatability(td) : previousIt) = true;
                             % points correspond to the generated tracked word
-                            BoTW.trackedWordPoints{twCounter} = trackedPointsBag{tw};
+                            BoTW.trackedWordPoints{twCounter} = trackedPointsBag{td};
                             % descriptors correspond to the generated tracked word
-                            BoTW.trackedWordDescriptors{twCounter} = trackedDescriptorsBag{tw};                
+                            BoTW.trackedWordDescriptors{twCounter} = trackedDescriptorsBag{td};                
                             % how many tracked words each location generates
-                            BoTW.lamda(1, BoTW.twIndex(twCounter, 1) : BoTW.twIndex(twCounter, 2)) =  BoTW.lamda(1, BoTW.twIndex(twCounter, 1) : BoTW.twIndex(twCounter, 2)) + 1;
+                            BoTW.lamda(1, BoTW.twIndex(twCounter, 1) : BoTW.twIndex(twCounter, 2)) =  BoTW.lamda(1, BoTW.twIndex(twCounter, 1) : BoTW.twIndex(twCounter, 2)) + 1;                                    
                         end
                         
                         % replacement of point that either became TW or just lose its track
-                        if trackObservation(tw) == false && newPointCounter < size(pointsToSearch, 1)
+                        if trackObservation(td) == false && newPointCounter < size(pointsToSearch, 1)
                             newPointCounter = newPointCounter + 1;
                             % new point addition
-                            BoTW.queryPoints{It}(tw, :) = pointsToSearch(newPointCounter, : );
+                            BoTW.queryPoints{It}(td, :) = pointsToSearch(newPointCounter, : );
                             % new query descriptor addition
-                            BoTW.queryDescriptors{It}(tw, :) = descriptorsToSearch(newPointCounter, : );
+                            BoTW.queryDescriptors{It}(td, :) = descriptorsToSearch(newPointCounter, : );
                             % new point addition
-                            trackedPointsBag{tw} = pointsToSearch(newPointCounter, : );
+                            trackedPointsBag{td} = pointsToSearch(newPointCounter, : );
                             % new descriptor addition
-                            trackedDescriptorsBag{tw} = descriptorsToSearch(newPointCounter, : );
+                            trackedDescriptorsBag{td} = descriptorsToSearch(newPointCounter, : );
                             % initialization new point representation
-                            pointRepeatability(tw) = 1;                        
+                            pointRepeatability(td) = 1;                        
                         % in cases where the plane is not textured enough                        
-                        elseif trackObservation(tw) == false && newPointCounter >= size(pointsToSearch, 1) && size(BoTW.queryPoints{It}, 1) >= tw
+                        elseif trackObservation(td) == false && newPointCounter >= size(pointsToSearch, 1) && size(BoTW.queryPoints{It}, 1) >= td
                             deletion = true;
-                            pointsToDelete(tw) = true;                        
+                            pointsToDelete(td) = true;                        
                         end
-                        
                     end
                     
                     if deletion == true
@@ -141,38 +139,44 @@ function BoTW = buildingDatabase(visualData, params)
                         trackedPointsBag((pointsToDelete == true), :) = [];
                         trackedDescriptorsBag((pointsToDelete == true), :) = [];
                         pointRepeatability((pointsToDelete == true), :) = [];
-                        pointRepeatability(length(pointRepeatability) + 1 : params.numPointsToTrack) = 1;
-                    end
-           
-                    BoTW.maximumActivePoint(It) = max(pointRepeatability);
-            
+                        pointRepeatability(length(pointRepeatability) + 1 : params.buildingDatabase.numPointsToTrack) = 1;
+                    end           
+                    
+                    BoTW.maximumActivePoint(It) = max(pointRepeatability);            
                 else
                     
-                    % initialization process again                    
-                    trackObservation = false(params.numPointsToTrack, 1);
-                    pointRepeatability = ones(params.numPointsToTrack, 1, 'int16');                          
+                    % initialization process if needed (cases with low textured images)
+                    trackObservation = false(params.buildingDatabase.numPointsToTrack, 1);
+                    pointRepeatability = ones(params.buildingDatabase.numPointsToTrack, 1, 'uint16');
                     
-                    if size(visualData.pointsSURF{It}, 1) > params.numPointsToTrack
-                        BoTW.queryPoints{It}= visualData.pointsSURF{It}.Location(1 : params.numPointsToTrack, :);                    
-                        trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, params.numPointsToTrack));                    
-                        BoTW.queryDescriptors{It} = visualData.featuresSURF{It}(1 : params.numPointsToTrack, :);                    
-                        trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, params.numPointsToTrack));
+                    if size(visualData.points{It}, 1) > params.buildingDatabase.numPointsToTrack
+                        BoTW.queryPoints{It}= visualData.points{It}.Location(1 : params.buildingDatabase.numPointsToTrack, :);
+                        trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, params.buildingDatabase.numPointsToTrack));
+                        BoTW.queryDescriptors{It} = visualData.features{It}(1 : params.buildingDatabase.numPointsToTrack, :);
+                        trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, params.buildingDatabase.numPointsToTrack));
                     % in case the image texture can not generate enough visual local features
-                    elseif size(visualData.pointsSURF{It}, 1) < params.numPointsToTrack ... 
-                            && size(visualData.pointsSURF{It}, 1) > params.inliersTheshold
-                        BoTW.queryPoints{It}= visualData.pointsSURF{It}.Location(1 : size(visualData.pointsSURF{It}, 1), :);
-                        trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, size(visualData.pointsSURF{It}, 1)));                   
-                        BoTW.queryDescriptors{It} = visualData.featuresSURF{It}(1 : size(visualData.featuresSURF{It}, 1), :);
-                        trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, size(visualData.featuresSURF{It}, 1)));                        
+                    elseif size(visualData.points{It}, 1) < params.buildingDatabase.numPointsToTrack ... 
+                            && size(visualData.points{It}, 1) > params.queryingDatabase.inliersTheshold
+                        BoTW.queryPoints{It}= visualData.points{It}.Location(1 : size(visualData.points{It}, 1), :);
+                        trackedPointsBag = mat2cell(BoTW.queryPoints{It}, ones(1, size(visualData.points{It}, 1)));                   
+                        BoTW.queryDescriptors{It} = visualData.features{It}(1 : size(visualData.features{It}, 1), :);
+                        trackedDescriptorsBag = mat2cell(BoTW.queryDescriptors{It}, ones(1, size(visualData.features{It}, 1)));                        
                     end
                     
                 end                
             end            
         end
         
+        BoTW.bagOfTrackedWords = BoTW.bagOfTrackedWords(1:twCounter, :);
+        BoTW.twIndex = BoTW.twIndex(1:twCounter, :);
+        BoTW.twLocationIndex = BoTW.twLocationIndex(1:twCounter, :);
+        BoTW.trackedWordPoints = BoTW.trackedWordPoints(1, 1 : twCounter);
+        BoTW.trackedWordDescriptors = BoTW.trackedWordDescriptors(1, 1 : twCounter);
+        
+        
         % save the generated database if not a file exists
         if params.buildingDatabase.save
-            save('results/buildingDatabase', 'BoTW', '-v7.3');
+            save('results/buildingDatabase', 'BoTW', 'timer', '-v7.3');
         end
         
     end
