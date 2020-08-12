1e3bf047226a80398177c7661a439af847ff83ee
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 18:27:19 +0300

    Update guidedFeatureDetection.m

--- matlab/guidedFeatureDetection.m
+++ matlab/guidedFeatureDetection.m
@@ -1,32 +1,52 @@
-function [pointsFedtoTracker, pointsDescriptors, pointRepeatability, trackObservation, pointsToSearch, descriptorsToSearch, trackedPointsBag, trackedDescriptorsBag] = ...
-    guidedFeatureDetection(params, visualData, previousImg, It, pointsFedtoTracker, pointsDescriptors, trackedPoints, trackedPointsValidity, pointRepeatability, trackedPointsBag, trackedDescriptorsBag, trackObservation)
-    
-    pointsToSearch = single(visualData.pointsSURF{It}.Location);        
-    descriptorsToSearch = visualData.featuresSURF{It};
-    % SURF Points in order to find the appropriate descriptor in previous image
-    SPprevious = KDTreeSearcher(visualData.pointsSURF{previousImg}.Location); 
-    excludedPoint = int16([]);
+% 
 
-    for j = 1 : params.numPointsToTrack
-        
+% Copyright 2019, Konstantinos Tsintotas
+% ktsintot@pme.duth.gr
+%
+% This file is part of Bag-of-Tracked-Words framework for loop closure
+% detection
+%
+% Bag-of-Tracked-Words framework is free software: you can redistribute 
+% it and/or modify it under the terms of the MIT License as 
+% published by the corresponding authors.
+%  
+% Bag-of-Tracked-Words pipeline is distributed in the hope that it will be 
+% useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% MIT License for more details. <https://opensource.org/licenses/MIT>
+
+function [pointsFedtoTracker, pointsDescriptors, pointRepeatability, trackObservation, pointsToSearch, descriptorsToSearch, trackedPointsBag, trackedDescriptorsBag, timer] = ...
+    guidedFeatureSelection(params, visualData, previousImg, It, pointsFedtoTracker, pointsDescriptors, trackedPoints, trackedPointsValidity, pointRepeatability, trackedPointsBag, trackedDescriptorsBag, trackObservation, timer)
+    
+    pointsToSearch = single(visualData.points{It}.Location);        
+    descriptorsToSearch = visualData.features{It};
+    excludedPoint = int16([]);
+    
+    % allocate the timer
+    guidedFeatureSelectionTiming = zeros(1, params.buildingDatabase.numPointsToTrack,'single');
+    
+    for j = 1 : params.buildingDatabase.numPointsToTrack
+                
         % exclusion of point that used before from guided point detection in order a duplicate to be avoided
         pointsToSearch(excludedPoint, :) = [];
         % exclusion of descriptor that used before from guided feature detection in order a duplicate to be avoided
         descriptorsToSearch(excludedPoint, :) = [];
-        % preparation of pointsToSearch database for k-NN
-        SP = KDTreeSearcher(pointsToSearch);
+        
+        % start the timer for the guided feature selection
+        tic
         
         % check if point of the previous image is tracked in the current and if the number of points are lower the desired            
         if j <= size(trackedPointsValidity, 1) && trackedPointsValidity(j) == 1 && ~isempty(pointsToSearch)
-
+                        
             % nearest neighbor index and points' distance between the tracked point "tp" and SURF points "SP" in I(t)
-            [IdxNN, pointsDist] = knnsearch(SP, trackedPoints(j, :), 'K', 1 );
+            [IdxNN, pointsDist] = knnsearch(pointsToSearch, trackedPoints(j, :), 'K', 1, 'NSMethod', 'kdtree');
+            % SURF Points nearest neighbor in order to find the appropriate descriptor in previous image
+            [p, ~] = knnsearch(visualData.points{previousImg}.Location, pointsFedtoTracker(j, :), 'K', 1, 'NSMethod', 'kdtree' );
             % descriptors' distance
-            [p, ~] = knnsearch(SPprevious, pointsFedtoTracker(j, :), 'K', 1 );
-            descriptorsDist= norm(visualData.featuresSURF{previousImg}(p, :) - descriptorsToSearch(IdxNN, :));
-
+            descriptorsDist= norm(visualData.features{previousImg}(p, :) - descriptorsToSearch(IdxNN, :));       
+            
             % two conditions for acceptance of a tracked point
-            if pointsDist < params.pointsDist && descriptorsDist < params.descriptorsDist
+            if pointsDist < params.buildingDatabase.pointsDist && descriptorsDist < params.buildingDatabase.descriptorsDist
                 % accepted point and descriptor
                 trackObservation(j) = true;
                 % to maintain the correct point detected in the current image                                     
@@ -40,21 +60,25 @@ function [pointsFedtoTracker, pointsDescriptors, pointRepeatability, trackObserv
                 % point repeatability along consecutive images
                 pointRepeatability(j) = pointRepeatability(j) + 1;
                 % point to be deleted from the repetitive function
-                excludedPoint = IdxNN;                
+                excludedPoint = IdxNN;        
             else                
                 trackObservation(j) = false; 
-                excludedPoint = [];
+                excludedPoint = [];        
             end
-
         else
             trackObservation(j) = false;      
             excludedPoint = [];
-        end   
+        end
+        
+        % stop the timer for the guided feature selection
+        guidedFeatureSelectionTiming(1, j) = toc;
         
     end
     
-    % exclusion of point and descriptor that used before from tracking device for
-    % avoidance of duplicate at the new points addition
+    guidedFeatureSelectionTiming = mean(guidedFeatureSelectionTiming);
+    timer.guidedFeatureSelection(It, 1) = guidedFeatureSelectionTiming;
+    
+    % exclusion of points and descriptors that used before from guided tracking device for avoidance of duplicate at the new points addition
     pointsToSearch(excludedPoint, :) = [];    
     descriptorsToSearch(excludedPoint, :) = []; 
     
